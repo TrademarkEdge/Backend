5d1fe5c4a2a6620be046e1ba158a8df56d4a5fa7
1103butt <135291748+Hassanbutt1103@users.noreply.github.com>
2025-10-02 18:07:15 +0500

    new-updates

--- index.js
+++ index.js
@@ -12,7 +12,11 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
 
-// Routes
+// Log every request
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.path} route hit`);
+  next();
+});
 
 // "/" route for Hello
 app.get("/", (req, res) => {
